251a06ea5eeae2a78ef249eb417a24a58279f4fd
Ukanwauzoma17 <lankerjunior13@gmail.com>
2024-07-18 15:24:24 +0100

    setup

---
+++ src/category/kora/kora-service.ts
@@ -0,0 +1,18 @@
+
+export const koraData=[
+    {
+        question: "Who is the Ceo of KoraPay",
+        option1: "Ukanwa Uzoma",
+        option2: "Dickson Nsofor",
+        option3: "Bishop David Oyedepo",
+        option4: "Martins Kayode",
+        ans: "option2",
+   },
+
+   {
+
+    }
+]
+
+
+
