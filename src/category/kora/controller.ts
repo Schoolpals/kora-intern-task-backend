37804675a13f3b28992b34094af8311ebafa6ef1
Ukanwauzoma17 <lankerjunior13@gmail.com>
2024-07-19 13:40:36 +0100

    fix made

--- src/category/kora/controller.ts
+++ src/category/kora/controller.ts
@@ -1,5 +1,4 @@
 import { getKoraQuizById } from './service';
-
 import { Request, Response } from "express";
 import Quiz from "../../quiz/model/quiz-model";
 import SuccessResponse from '../../utils/success-response';
@@ -7,7 +6,7 @@ import Kora from './model/kora-model';
 
 export const kora = async(req:Request,res:Response):Promise<void>=>{
 const {quesId,quizId}=req.body
-const koraId=await Kora.findOne({where:{id:quesId}})
+const koraId=await Kora.findOne({where:{quesId}})
 if(!koraId){
    res.status(400).json({message:"Error in finding quiz"})
 }
