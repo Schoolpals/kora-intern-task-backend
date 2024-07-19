c2e46ededf66f2f7f8f96c3edb1d87196609c9df
Ukanwauzoma17 <lankerjunior13@gmail.com>
2024-07-19 21:25:08 +0100

    fix made

---
+++ config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'database_dev',
+    password: 'database_dev',
+    database: 'database_dev',
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    }
+}
