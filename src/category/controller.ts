d9957c891f9340cb27ad863e4cef757d28a46f57
Ukanwauzoma17 <lankerjunior13@gmail.com>
2024-07-21 00:58:24 +0100

    user upload building

--- src/category/controller.ts
+++ src/category/controller.ts
@@ -5,20 +5,32 @@ import { generateCategoryId } from "../utils/generate-category-id";
 import ErrorResponse from "../utils/error-response";
 import { findByUserId } from "../users/service/user-service";
 import Error404Response from "../utils/error-response";
+import { generateQuizId } from "../utils/generate-quiz-id";
 
-export const createCategory = async (req: Request, res: Response): Promise<void> => {
-    const userId = req.user?.id
-    if (!userId) {return}
-    const userExist = findByUserId(userId)
-    if (!userExist) {
-        Error404Response.send(res,{error: "User Not Found"})
-      }
-  const { categoryName } = req.body;
-  if (!categoryName) {
-    ErrorResponse.send(res,{ message: "Category name is required" });
+export const createCategory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const userId = req.user?.id;
+  const categoryId = generateCategoryId();
+  const quizId = generateQuizId();
+  if (!userId) {
     return;
   }
-    const newCategory = await Category.create({categoryName});
-    const categoryId = await generateCategoryId
-    SuccessResponse.send(res, {message:{newCategory, categoryId}});
+  const userExist = findByUserId(userId);
+  if (!userExist) {
+    Error404Response.send(res, { error: "User Not Found" });
+  }
+  const { categoryName } = req.body;
+  if (!categoryName) {
+    ErrorResponse.send(res, { message: "Category name is required" });
+    return;
+  }
+  const newCategory = await Category.create({
+    categoryName,
+    categoryId,
+    quizId,
+  });
+
+  SuccessResponse.send(res, { message: { newCategory } });
 };
